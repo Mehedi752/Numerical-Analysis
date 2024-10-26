01feebdc7375933dc7f044c1e6307bec7d000d23
Mehedi <mehedihasansagor301@example.com>
2024-10-26 15:44:49 +0600

    starting numerical analysis course

---
+++ bisectionMethod.cpp
@@ -0,0 +1,28 @@
+// Mehedi Hasan @Mehedi752
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define lld long double
+#define MOD 1000000007
+
+void Mehedi()
+{
+    ll n;
+    cin >> n;
+    vector<ll> arr(n);
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    
+    
+}
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++)
+        Mehedi();
+    return 0;
+}
